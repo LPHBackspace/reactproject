fb222b2541d27e817ec9e3f1fbc52cb66f3bded4
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2023-08-16 19:45:58 -0300

    front v3

--- react-website-yt-1/src/App.js
+++ react-website-yt-1/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
+import Home from './components/pages/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,9 +8,9 @@ function App() {
     <>
       <Router>
         <Navbar/>
-        <HeroSection/>
+        <Home/>
         <Routes>
-          <Route path='/' exact/>
+          <Route path='/' exact component={Home}/>
         </Routes>
       </Router>
     </>
--- react-website-yt-1/src/components/HeroSection.js
+++ react-website-yt-1/src/components/HeroSection.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Button } from './Button';
+import './HeroSection.css';
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-        <img src='.../public/images/goku.jpg'></img>
-    </div>
+    <>
+      <div className='hero-container'>
+
+        <div className='hero-img'></div>
+
+        <div className='hero-content'>
+          
+          <h1>DRAGONBALL FIGHTERZ</h1>
+          <p>O melhor jogo de luta anime</p>
+
+          <div className='hero-btns'>
+            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
+            <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>WHAT TRAILER<ion-icon name="play-circle-outline"></ion-icon></Button>
+          </div>
+
+        </div>
+
+      </div>
+    
+    </>
   )
 }
 
